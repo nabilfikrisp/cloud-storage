7982c36b87b3c371ede13754fcdde950b7338def
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-10-01 17:37:45 +0700

    feat: configure ESLint and Prettier for frontend and backend, add Tailwind CSS plugin

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Cloud Storage</h1>
+      <h1 className="bg-red-200 text-4xl">Welcome to Cloud Storage</h1>
       <Button>Get Started</Button>
     </div>
   );
