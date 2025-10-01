a1eba19543b43b9ac4c3b37be5425dd9e5411c72
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-10-01 16:47:04 +0700

    chore: init nest js and next js with pnpm workspace

---
+++ apps/backend/eslint.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["eslint.config.mjs", "dist/**", "node_modules/**"],
+  },
+  ...tseslint.configs.recommended, // TS base rules
+  ...tseslint.configs.recommendedTypeChecked, // strict rules w/ type info
+  eslintPluginPrettierRecommended, // Prettier plugin + config
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: "commonjs",
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+    },
+  },
+];
---
+++ apps/backend/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3001);
+}
+
+bootstrap().catch((err) => {
+  console.error("Error during bootstrap:", err);
+  process.exit(1);
+});
