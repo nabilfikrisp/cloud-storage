a851332f3a1a87895a0926ac10ba94487a98c72f
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-10-04 15:34:26 +0700

    chore: simplify project structure

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
-import { Prisma, User } from "generated/prisma";
 import { PrismaService } from "../prisma/prisma.service";
+import { Prisma, User } from "@prisma-generated";
 
 @Injectable()
 export class UserService {
